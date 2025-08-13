a8385ea6aa584dee0fdb76ae77b97e9b0caf4776
zihao.chen <zihao.chen@xilancloud.com>
2025-08-13 14:48:33 +0800

    feat:add Swiper component

---
+++ src/components/Swiper.vue
@@ -0,0 +1,21 @@
+<template>
+  <van-swipe :autoplay="3000" lazy-render>
+    <van-swipe-item v-for="item in data" :key="item.id">
+      <div class="flex flex-col items-center w-full h-full py-8">
+        <img :src="item.cover" class="object-contain w-full h-40" />
+        <div class="text-black">{{ item.title }}</div>
+      </div>
+    </van-swipe-item>
+  </van-swipe>
+</template>
+<script setup lang="ts">
+import type { NewsList } from '@/types/news';
+import { defineProps, PropType } from 'vue';
+
+const props = defineProps({
+  data: {
+    type: Array as PropType<NewsList>,
+    default: () => []  // 注意这里必须用函数返回
+  }
+})
+</script>
