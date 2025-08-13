fd38653a46728e3c4a9b4e1097c6eb6889904888
zihao.chen <zihao.chen@xilancloud.com>
2025-08-13 14:59:38 +0800

    feat:update tabbar data render

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <RouterView />
-  <van-tabbar v-model="active" active-color="#ee0a24">
-    <van-tabbar-item icon="home-o"><RouterLink to="/"/>Home</van-tabbar-item>
-    <van-tabbar-item icon="search">Tab</van-tabbar-item>
-    <van-tabbar-item icon="friends-o">Tab</van-tabbar-item>
-    <van-tabbar-item icon="setting-o"><RouterLink to="/settings"/>Settings</van-tabbar-item>
+  <van-tabbar v-model="active" active-color="#ee0a24" route>
+    <van-tabbar-item v-for="item in tabbar" :icon="item.icon" :to="item.link">{{ item.title }}</van-tabbar-item>
   </van-tabbar>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
  const active = ref(0)
+ const tabbar = ref([
+  {
+    title:'首页',
+    icon:'home-o',
+    link:'/'
+  },{
+    title:'计划',
+    icon:'search'
+  },{
+    title:'群组',
+    icon:'friends-o'
+  },{
+    title:'个人信息',
+    icon:'setting-o',
+    link:'/info'
+  },
+ ])
 </script>
 <style></style>
